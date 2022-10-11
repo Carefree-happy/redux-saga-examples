c8dbb79ed3b05d27df0d40699d2a7d3cd8efb48d
sunjing <sunjing@mzhcorp.com>
2022-10-11 15:39:37 +0800

    feat: counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react"
+import ReactDOM from "react-dom"
+import { createStore, applyMiddleware } from "redux"
+import createSagaMiddleware from "redux-saga"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Counter from "./components/Counter"
+import reducer from "./reducers"
+import rootSaga from "./sagas"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(rootSaga)
+
+const action = (type) => store.dispatch({ type })
+
+function render() {
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() => action("INCREMENT")}
+            onDecrement={() => action("DECREMENT")}
+            onIncrementIfOdd={() => action("INCREMENT_IF_ODD")}
+            onIncrementAsync={() => action("INCREMENT_ASYNC")}
+        />,
+        document.getElementById("root")
+    )
+}
+
+render()
+store.subscribe(render)
