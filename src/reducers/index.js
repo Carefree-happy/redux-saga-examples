b0ffa4390737341ef0398fb548713aef90b438ef
sunjing <sunjing@mzhcorp.com>
2022-10-12 11:44:40 +0800

    ci: clean dev

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { countdown, counter } from "./counter";
-
-const rootReducer = combineReducers({
-  countdown,
-  counter,
-});
-
-export default rootReducer;
